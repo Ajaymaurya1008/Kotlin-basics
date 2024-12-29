8e773c47a2fed6d9cd853452bb097dfd9a110c4a
Ajaymaurya1008 <ajaykvmaurya@gmail.com>
2024-12-29 15:57:25 +0530

    init

---
+++ src/Main.kt
@@ -0,0 +1,33 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+//    var arr = arrayOf(1,2,3,4,5)
+//    for((i,e) in arr.withIndex()) {
+//        println(e)
+//    }
+
+//    for(i in 1..10)println(i)
+
+//    for (i in 5 downTo 1) println(i)
+
+//    for (i in 20 downTo 1 step 2) println(i)
+
+//    for (i in 'd'..'m') print(i + " ")
+
+//    var count = 0
+//
+//    while (count <= 10) {
+//        println(count)
+//        count++
+//    }
+//
+//    do {
+//        println(count)
+//        count--
+//    } while (count >= 0)
+
+//    repeat(10){
+//        println("Hola, mi amigos")
+//    }
+
+}
